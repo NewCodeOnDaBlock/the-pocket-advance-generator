5d8cee1931581f2080a799bf54a2377c2bf9c334
radenlmantuano <newcodeondablock@gmail.com>
2026-01-21 22:40:45 -0800

    maintsx change

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App";
 import "./index.css";
-import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
